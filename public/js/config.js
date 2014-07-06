e88c8d692387f6ee25342a3aa601896f37ee676c
Jawaid Iqbal <jawaid@cygnismedia.com>
2014-07-06 17:12:15 +0500

    Setting up correct paths

--- public/js/config.js
+++ public/js/config.js
@@ -3,36 +3,51 @@ var server		= window.location.hostname;
 var canvas_url    = "";
 if (server=='localhost')
 {
-	canvas_url       = location.protocol+"//localhost/wildme-v2/public/";
-	AdminUrl		 = location.protocol+"//localhost/wildme-v2/public/cpanel/";
+	canvas_url       = location.protocol+"//localhost/wildme/public/";
+	AdminUrl		 = location.protocol+"//localhost/wildme/public/cpanel/";
+}
+else if (server=='prod2.cygnismedia.com')
+{
+	canvas_url       = location.protocol+"//prod2.cygnismedia.com/wildme/public/";
+	AdminUrl		 = location.protocol+"//prod2.cygnismedia.com/wildme/public/cpanel/";
+}
+else if (server=='prod.cygnismedia.com')
+{
+	canvas_url       = location.protocol+"//prod.cygnismedia.com/wildme/public/";
+	AdminUrl		 = location.protocol+"//prod.cygnismedia.com/wildme/public/cpanel/";
 }
 else if (server=='digital.cygnismedia.com')
 {
-	canvas_url       = location.protocol+"//digital.cygnismedia.com/wildme-v2/public/";
-	AdminUrl		 = location.protocol+"//digital.cygnismedia.com/wildme-v2/public/cpanel/";
+	canvas_url       = location.protocol+"//digital.cygnismedia.com/facebook/wildme/public/";
+	AdminUrl		 = location.protocol+"//digital.cygnismedia.com/facebook/wildme/public/cpanel/";
 }
 else if (server=='wildme.cygnismedia.com')
 {
 	canvas_url       = location.protocol+"//wildme.cygnismedia.com/wildme/public/";
 	AdminUrl		 = location.protocol+"//wildme.cygnismedia.com/wildme/public/cpanel/";
 }
-else //if (server=='fb.wildme.org')
+else if (server=='fb.wildme.org')
 {
 	canvas_url       = location.protocol+"//fb.wildme.org/wildme/public/";
 	AdminUrl		 = location.protocol+"//fb.wildme.org/wildme/public/cpanel/";
 }
+else 
+{
+	canvas_url  = location.protocol+"//developer.cygnismedia.com/facebook/wildme/public/";
+	AdminUrl	= location.protocol+"//developer.cygnismedia.com/facebook/wildme/public/cpanel/";
+}
 
 var API_URL    = canvas_url+"api/";
 
-var canvasPage = "http://apps.facebook.com/wildbook/";
+var canvasPage = "http://apps.facebook.com/wildmeapp/";
 
 var cUrl 		= window.location;
 var sPath 		= window.location.pathname;
 var sPage 		= sPath.substring(sPath.lastIndexOf('/') + 1);   //if(sPage == 'index.php')
 
-var applicationId     = "306566776167234"; //414751805313094
-var applicationSecret = "fa214567c0c9266f61908498ff9e7325"; //f65a47c37be9e536f7003649dbc3a851
-var APP_URL	=	'http://apps.facebook.com/wildbook/'; //wildmeapp
+var applicationId     = "414751805313094";
+var applicationSecret = "f65a47c37be9e536f7003649dbc3a851";
+var APP_URL	=	'http://apps.facebook.com/wildmeapp/';
 
 var session_key;
 var access_token;
@@ -48,4 +63,4 @@ var access_token=0;
 var API_URL = canvas_url+'api/';
 
 
-var appInstallUrl = "https://www.facebook.com/dialog/oauth/?client_id="+applicationId+"&redirect_uri="+canvasPage+"&scope=email,publish_actions,publish_stream,offline_access";
+var appInstallUrl = "https://www.facebook.com/dialog/oauth/?client_id="+applicationId+"&redirect_uri="+canvasPage+"&scope=email";
